478bb776d4762732286392b64eb688433a902e4d
Noxyx <68758078+MrNoxy@users.noreply.github.com>
2026-01-12 21:41:26 +0200

    Create firebaseConfig.js

---
+++ v3/firebaseConfig.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCfntkpwE_ZmBmujI8MpuSYulQCXCyu9sM",
+  authDomain: "spaceeasy-scripthub.firebaseapp.com",
+  databaseURL: "https://spaceeasy-scripthub-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "spaceeasy-scripthub",
+  storageBucket: "spaceeasy-scripthub.firebasestorage.app",
+  messagingSenderId: "64540262694",
+  appId: "1:64540262694:web:5e8afbe794c7e0dc9401e7"
+};
